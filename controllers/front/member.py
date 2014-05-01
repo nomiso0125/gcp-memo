c70d7be72f7d327bfcbd22c758575c4a01b38aa8
nomiso0125 <nomiso0125@gmail.com>
2014-05-01 16:17:38 +0900

    Cloud Strage

--- controllers/front/member.py
+++ controllers/front/member.py
@@ -1,10 +1,10 @@
 #!-*- coding:utf-8 -*-
 #!/usr/bin/env python
 
-
 from lib.controller import *
 from google.appengine.api import users
-
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 class Top(Controller):
     def get(self):
@@ -20,9 +20,22 @@ class Top(Controller):
 
         self.draw_template('front/member/top.html')
 
+class Movie(Controller):
+    def get(self):
+        self.draw_template('front/member/movie.html')
+
+
+class PlayMovie(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self):
+
+        blob_key = blobstore.create_gs_key('/gs/gcp-memo.appspot.com/movies/meteorite.mp4')
+
+        self.send_blob(blob_key)
 
 url_map = [
     ('/member/top', Top),
+    ('/member/movie', Movie),
+    ('/member/play_movie', PlayMovie),
 ]
 
 application = webapp.WSGIApplication(url_map, debug=True)
